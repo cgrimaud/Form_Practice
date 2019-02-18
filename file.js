a17687b9ae696284a7e7878eccd1f603a5957380
Christy Grimaud <christy.grimaud@gmail.com>
2019-02-18 17:40:49 -0500

    removed extraneous comments

--- file.js
+++ file.js
@@ -31,22 +31,14 @@ $(document).ready(function(){
 // Form Validaiton 
 
 $(function() {
-    // Initialize form validation on the registration form.
-    // It has the name attribute "registration"
     $("form[name='helpDeskTicket']").validate({
-      // Specify validation rules
       rules: {
-        // The key name on the left side is the name attribute
-        // of an input field. Validation rules are defined
-        // on the right side
         userName: {
             required: true,
             minlength: 3
         },        
         userEmail: {
           required: true,
-          // Specify that email should be validated
-          // by the built-in "email" rule
           email: true
         },
         userId: {
@@ -54,14 +46,11 @@ $(function() {
             minlength: 4
         },   
       },
-      // Specify validation error messages
       messages: {
         userName: "You must enter your name (minimum 3 characters)",
         userEmail: "Please provide a valid email address",
         userId: "You must enter your USI User ID"
       },
-      // Make sure the form is submitted to the destination defined
-      // in the "action" attribute of the form when valid
       submitHandler: function(form) {
         console.log(form);
         form.submit();
